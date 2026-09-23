3b4fbdb61c199a2d9bdb756d0590e6d4a31c723d
Shevagar <41564751+Shevagar@users.noreply.github.com>
2026-09-23 21:53:14 +0530

    Normalize escaped newlines in CycloneDX exporter

--- cbom_analyzer/cyclonedx.py
+++ cbom_analyzer/cyclonedx.py
@@ -21,7 +21,10 @@ def _key_ref(k,i):
 
 def _algorithm_component(a,i):
     ref=_algorithm_ref(a,i)
-    algorithm_properties={"primitive":_primitive(a)}\n    family=ALGORITHM_FAMILIES.get(a.name)\n    if family: algorithm_properties["algorithmFamily"]=family\n    props={"assetType":"algorithm","algorithmProperties":algorithm_properties}
+    algorithm_properties={"primitive":_primitive(a)}
+    family=ALGORITHM_FAMILIES.get(a.name)
+    if family: algorithm_properties["algorithmFamily"]=family
+    props={"assetType":"algorithm","algorithmProperties":algorithm_properties}
     if a.key_size: props["algorithmProperties"]["parameterSetIdentifier"]=str(a.key_size)
     funcs=_functions(a)
     if funcs: props["algorithmProperties"]["cryptoFunctions"]=funcs
